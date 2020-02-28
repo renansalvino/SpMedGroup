a2ccdb23d73da1c2cdca7d6aa36cc877287146d9
renansalvino <renansalvinomartins454@gmail.com>
2020-02-28 02:41:28 -0300

    Inicialização da API

---
+++ SPMedicalGroup/SPMedicalGroup/Controllers/Clinica.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPMedicalGroup.Controllers
+{
+    public class Clinica
+    {
+    }
+}
