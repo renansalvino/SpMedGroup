6a798f301f4f833a0abfb652b018fe1422bd7e8d
Renansalvino <renansalvinomartins454@gmail.com>
2020-10-11 17:19:37 -0300

    Para formatar

--- Scripts/SpMedGroup-01-DDL.sql
+++ Scripts/SpMedGroup-01-DDL.sql
@@ -2,16 +2,17 @@ CREATE DATABASE SpMedGroup
 
 USE SpMedGroup
 
+--Tabelas de Telefone
 CREATE TABLE Telefone (
 IdTelefone INT PRIMARY KEY IDENTITY,
 NumTelefone VARCHAR (255) NOT NULL
 );
-
+--Tabela de endereços
 CREATE TABLE Endereço (
 IdEndereco	INT PRIMARY KEY IDENTITY,
 Endereco	VARCHAR (255) NOT NULL UNIQUE
 );
-
+--Tabela de clínicas 
 CREATE TABLE Clinica (
 IdClinica		INT PRIMARY KEY IDENTITY,
 Clinica			VARCHAR (255)NOT NULL, 
@@ -23,17 +24,17 @@ IdTelefone INT FOREIGN KEY REFERENCES Telefone (IdTelefone )
 
 
 
-
+--Tabela der tipos de usuarios --
 CREATE TABLE TiposUsuarios (
 IdTiposUsuarios		INT PRIMARY KEY IDENTITY,
 TipoUsuario			VARCHAR (255) NOT NULL UNIQUE
 );
-
+-- Tabelas de especialidades 
 CREATE TABLE Especialidades (
 IdEspecialidades		INT PRIMARY KEY IDENTITY,
 TipoEspecialidade		VARCHAR (255) NOT NULL UNIQUE
 );
-
+-- Tabela de administrador
 CREATE TABLE Administrador (
 IdAdministrador		INT PRIMARY KEY IDENTITY,
 Nome				VARCHAR (255)NOT NULL,
@@ -42,7 +43,7 @@ Email				VARCHAR (255)NOT NULL,
 CPF					VARCHAR (255)NOT NULL,
 IdTiposUsuarios		INT FOREIGN KEY REFERENCES TiposUsuarios (idTiposUsuarios)
 );
-
+-- Tabela de médico
 CREATE TABLE Medico (
 IdMedico		INT PRIMARY KEY IDENTITY,
 Nome			VARCHAR (255)NOT NULL,
@@ -53,7 +54,7 @@ IdEspecialidades	INT FOREIGN KEY REFERENCES Especialidades (IdEspecialidades),
 IdClinica			INT FOREIGN KEY REFERENCES Clinica (IdClinica)
 );
 
-
+--Tabelas de Prontuários
 CREATE TABLE Prontuarios (
 IdProntuarios	INT PRIMARY KEY IDENTITY,
 Nome			VARCHAR (255)NOT NULL,
@@ -65,18 +66,14 @@ CPF				VARCHAR (255) NOT NULL UNIQUE,
 Endereco		VARCHAR (255) NOT NULL,
 IdTiposUsuarios INT FOREIGN KEY REFERENCES TiposUsuarios (IdTiposUsuarios)
 );
-
-
-
+--Tabelas de consulta
 CREATE TABLE Consulta (
 IdConsulta		INT PRIMARY KEY IDENTITY,
-Nome			VARCHAR (255) NOT NULL,
 DataConsulta	DATE NOT NULL UNIQUE,
 Situacao		VARCHAR (255) NOT NULL,
+Descrição		Text,
 IdMedico		INT FOREIGN KEY REFERENCES Medico (IdMedico),
-IdAdministrador	INT FOREIGN KEY REFERENCES Administrador (IdAdministrador),
-IdProntuarios	INT FOREIGN KEY REFERENCES Prontuarios (IdProntuarios)
+IdProntuarios	INT FOREIGN KEY REFERENCES Prontuarios (IdProntuarios),
+IdAdministrador INT FOREIGN KEY REFERENCES Administrador (IdAdministrador)
 );
-
-CREATE TABLE Usuarios 
 
